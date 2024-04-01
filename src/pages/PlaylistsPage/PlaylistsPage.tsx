d7e061b96e1ea4d681cc75657141cbd185914175
Nikita Mishutin <mishutin.ns@gmail.com>
2024-04-01 15:04:10 +0300

    replace use params with use search params

--- src/pages/PlaylistsPage/PlaylistsPage.tsx
+++ src/pages/PlaylistsPage/PlaylistsPage.tsx
@@ -1,33 +1,27 @@
 import { default as React, FC } from "react";
 import styles from "./playlistspage.css";
 import { PLAYLISTS } from "@data/playlists";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export const PlaylistsPage: FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const genre = queryParams.get("genre") || "";
-  const name = queryParams.get("name") || "";
-
-  function updateQueryParams(key: string, value: string): void {
-    queryParams.set(key, value);
-    const newSearch = `?${queryParams.toString()}`;
-    navigate({ search: newSearch });
-  }
+  const [searchParams, setSearchParams] = useSearchParams();
+  const listGenre = searchParams.get("listGenre") || "";
+  const listName = searchParams.get("listName") || "";
 
   function handleGenreSearch(event: React.ChangeEvent<HTMLInputElement>): void {
-    updateQueryParams("genre", event.target.value);
+    searchParams.set("listGenre", event.target.value);
+    setSearchParams(searchParams);
   }
   function handleNameSearch(event: React.ChangeEvent<HTMLInputElement>): void {
-    updateQueryParams("name", event.target.value);
+    searchParams.set("listName", event.target.value);
+    setSearchParams(searchParams);
   }
 
   const filteredPlaylists = PLAYLISTS.filter(
     list =>
       list.genre != "Non Music" &&
-      list.genre.toLowerCase().includes(genre.toLowerCase()) &&
-      list.name.toLowerCase().includes(name.toLowerCase()),
+      list.genre.toLowerCase().includes(listGenre.toLowerCase()) &&
+      list.name.toLowerCase().includes(listName.toLowerCase()),
   );
   return (
     <div className={styles.PlaylistsPage}>
